6ba6cd6c8c3b2f31b6a09570476391a85fc5addc
Oleg Mayorov <oleg.maiorov@gmail.com>
2020-11-09 16:04:43 +0300

    add params to PrintResult

--- app/src/main/java/ru/ucs/android/paymentservice/PrintResult.kt
+++ app/src/main/java/ru/ucs/android/paymentservice/PrintResult.kt
@@ -2,6 +2,6 @@ package ru.ucs.android.paymentservice
 
 sealed class PrintResult
 
-object PrintComplete: PrintResult()
+data class PrintComplete(val fiscalDocument: String, val transactionId: String, val shiftNumber: String): PrintResult()
 
 data class PrintError(val errorCode: Int, val errorMsg: String): PrintResult()
