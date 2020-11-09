530206bd2f6b0193739e30b1d841511e8d32fc28
Oleg Mayorov <oleg.maiorov@gmail.com>
2020-11-09 13:23:10 +0300

    tax rate is String

--- app/src/main/java/ru/ucs/android/paymentservice/entity/UcsTax.kt
+++ app/src/main/java/ru/ucs/android/paymentservice/entity/UcsTax.kt
@@ -3,7 +3,7 @@ package ru.ucs.android.paymentservice.entity
 data class UcsTax(
     val type: String,
     val name: String,
-    val rate: Double,
+    val rate: String,
     val included: Boolean,
     val amount: Double
 )
